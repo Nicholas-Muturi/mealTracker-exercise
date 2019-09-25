97c79dc142fb850b5bb1ffe4b5d4ee098d29ff6f
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-25 12:57:53 +0300

    inserted required properties in meal class

--- src/app/meal.ts
+++ src/app/meal.ts
@@ -1,2 +1,11 @@
 export class Meal {
+    showDetails: boolean;
+    constructor(
+        public id: number,
+        public name: string,
+        public calories: number,
+        public details: string
+    ){
+        this.showDetails = false;
+    }
 }
