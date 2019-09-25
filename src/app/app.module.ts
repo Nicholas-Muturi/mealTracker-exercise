6e0e1dbd2e59fcefbb4de30d74ff97438668db2d
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-25 12:54:56 +0300

    created basic components | created html skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from  '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MealDetailComponent } from './meal-detail/meal-detail.component';
+import { MealFormComponent } from './meal-form/meal-form.component';
+import { MealComponent } from './meal/meal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MealDetailComponent,
+    MealFormComponent,
+    MealComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
